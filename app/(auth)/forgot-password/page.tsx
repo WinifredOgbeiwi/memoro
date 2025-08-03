df8bb1167d442a1b2bc212dd15cc246c2145a17e
winifredogbeiwi <imadeogbeiwi@gmail.com>
2025-08-03 23:29:51 +0200

    Implement forgot password form

--- app/(auth)/forgot-password/page.tsx
+++ app/(auth)/forgot-password/page.tsx
@@ -1,9 +1,76 @@
-import React from 'react'
+"use client";
+import Button from "@/app/components/Button";
+import { Input } from "@/app/components/Input";
+import { IMAGES, ROUTES } from "@/app/lib/utils";
+import { useTheme } from "next-themes";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
-const ForgotPasswordPage = () => {
+const LoginPage = () => {
+const [email, setEmail] = React.useState("");
+
+  const { theme } = useTheme();
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log("User Email:", email);
+    // Handle login logic here
+  };
+
   return (
-    <div>ForgotPasswordPage</div>
-  )
-}
+    <section className="flex flex-col justify-center w-full  px-5 md:px-16 gap-5 h-full  ">
+      <Link href={ROUTES.home}>
+        <Image
+          src={theme === "dark" ? IMAGES.Logo_White : IMAGES.Logo}
+          alt="Memoro_auth_bg"
+          width={80}
+          height={80}
+          className=""
+          priority={true}
+        />
+      </Link>
 
-export default ForgotPasswordPage
+      <div>
+        <h1 className="font-bold text-3xl mb-2.5"> Forgot Your Password?</h1>
+        <p>Enter the email address linked to your account, and we will send you a password reset link.</p>
+      </div>
+      
+      <form action="" className="flex flex-col gap-4" onSubmit={handleSubmit}>
+     
+       <Input
+          label="Email"
+          name="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          type="email"
+          placeholder="Enter your email"
+        />
+
+        <Button style="mt-2" type="submit" text="Reset" />
+      </form>
+      <div className="flex  justify-between items-center text-sm gap-4 xs:flex-row flex-col xs:items-start">
+        <p className="text-text-secondary">
+          Don&apos;t have an account?{" "}
+          <Link
+            href={ROUTES.register}
+            className="text-primary2 hover:underline hover:scale-105"
+          >
+            Register
+          </Link>
+        </p>
+  <p className="text-text-secondary">
+          Remember password?{" "}
+          <Link
+            href={ROUTES.register}
+            className="text-primary2 hover:underline hover:scale-105"
+          >
+            Login
+          </Link>
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default LoginPage;
--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -3,7 +3,7 @@ import { ButtonProps } from '../type'
 
 const Button = ({style,type,text}:ButtonProps) => {
   return (
-           <button type={type} className={`bg-primary2 text-white rounded-md p-2 font-bold text-lg ${style} hover:bg-primary1 hover:scale-105 transition-colors duration-300`}>
+           <button type={type} className={`bg-primary2 text-surface rounded-md p-2 font-bold text-lg ${style} hover:bg-primary1 hover:scale-105 transition-colors duration-300`}>
          {text}
         </button>
   )
